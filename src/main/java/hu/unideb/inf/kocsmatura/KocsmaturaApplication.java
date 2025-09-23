c505172252c7e5c016874811471aa90926993ffc
Máté <szabo.mate@inf.unideb.hu>
2025-09-23 17:29:26 +0200

    init

---
+++ src/main/java/hu/unideb/inf/kocsmatura/KocsmaturaApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.kocsmatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KocsmaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KocsmaturaApplication.class, args);
+	}
+
+}

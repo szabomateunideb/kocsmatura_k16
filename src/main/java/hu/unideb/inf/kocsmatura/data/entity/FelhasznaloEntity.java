ae45ca606995566a3c7e03d4791e50189509d1cf
Máté <szabo.mate@inf.unideb.hu>
2025-09-30 16:42:33 +0200

    felhasznaloEntity, dbconfig

---
+++ src/main/java/hu/unideb/inf/kocsmatura/data/entity/FelhasznaloEntity.java
@@ -0,0 +1,32 @@
+package hu.unideb.inf.kocsmatura.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class FelhasznaloEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String nev;
+    @Column(name = "date_of_birth", nullable = false)
+    private Date szuletesiDatum;
+    @Column(name = "sex")
+    private String nem;
+    @Column(name = "username")
+    private String felhasznalonev;
+    @Column(name = "password")
+    private String jelszo;
+}
